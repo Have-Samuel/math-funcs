83172815ce1af8013e7d32e78c11917fb465535f
Have Samuel <samiehave9@gmail.com>
2022-12-17 02:15:00 +0300

    Implementing the core Game Ligic

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let gameIsRunning = false; // Stops any other game from starting
 
-function getPlayerChoice() {
+function getPlayerChoice() { // Player choice
   const selection = prompt(`${ROCK}, ${PAPER},${SCISSORS}?`, '').toUpperCase();
   if (
     selection !== ROCK
@@ -20,6 +23,27 @@ function getPlayerChoice() {
   return selection;
 }
 
+function getComputerChoice() { // computer choice
+  const randonValue = Math.random();
+  if (randonValue < 0.34) {
+    return ROCK;
+  } else if (randonValue < 0.64) {
+    return PAPER;
+  } else SCISSORS;
+}
+
+const getWinner = function (cChoice, pChoice) {
+  if (cChoice === pChoice) {
+    return RESULT_DRAW;
+  } else if (cChoice === ROCK && pChoice === PAPER
+    || cChoice === PAPER && pChoice === SCISSORS
+    || cChoice === SCISSORS && pChoice === ROCK) {
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+};
+
 startGameBtn.addEventListener('click', function() {
   if (gameIsRunning) {
     return;
@@ -27,5 +51,7 @@ startGameBtn.addEventListener('click', function() {
   gameIsRunning = true;
   console.log('The Game starts!...');
   const playerChoice = getPlayerChoice();
-  console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
 });
