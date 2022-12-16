b94c4eef4803894ca709c458826285ce84012ef1
Have Samuel <samiehave9@gmail.com>
2022-12-17 01:40:16 +0300

    Add user chioce to the Game

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function () {
-//   console.log('The Game starts!...');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
 
-// const greet = {
-//   greet: function what() {
-//     console.log('Hello There!');
-//   }
-// };
+let gameIsRunning = false; // Stops any other game from starting
 
-// console.dir(startGame);
-
-// greet.greet();
+function getPlayerChoice() {
+  const selection = prompt(`${ROCK}, ${PAPER},${SCISSORS}?`, '').toUpperCase();
+  if (
+    selection !== ROCK
+    && selection !== PAPER
+    && selection !== SCISSORS
+  ) {
+    alert(`Invalid input!, ${DEFAULT_USER_CHOICE} was selected for you.`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+}
 
 startGameBtn.addEventListener('click', function() {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log('The Game starts!...');
+  const playerChoice = getPlayerChoice();
+  console.log(playerChoice);
 });
